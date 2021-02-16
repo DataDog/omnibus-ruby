421b589070962f325a4cb32af5dd9450edc3e6ea
Tom Duffield <tom@chef.io>
2021-02-16 16:43:48 -0600

    Add support for macOS 11 and multiple architectures
    * Provide the correct platform version (11 vs 11.x) for macOS.
    * Add the architecture (x86_64, arm64) to the file name
    
    Cherry-picked-from: chef/omnibus@2b1b503fc7e532771788be349e597c2798cb401a
    Co-authored-by: Régis Desgroppes <regis.desgroppes@datadoghq.com>

--- lib/omnibus/packagers/pkg.rb
+++ lib/omnibus/packagers/pkg.rb
@@ -146,7 +146,7 @@ module Omnibus
 
     # @see Base#package_name
     def package_name
-      "#{safe_base_package_name}-#{safe_version}-#{safe_build_iteration}.pkg"
+      "#{safe_base_package_name}-#{safe_version}-#{safe_build_iteration}.#{safe_architecture}.pkg"
     end
 
     #
@@ -274,6 +274,15 @@ module Omnibus
     #
     def component_pkg
       "#{safe_base_package_name}-core.pkg"
+    end
+
+    #
+    # Return the architecture
+    #
+    # @return [String]
+    #
+    def safe_architecture
+      @safe_architecture ||= Ohai["kernel"]["machine"]
     end
 
     #
