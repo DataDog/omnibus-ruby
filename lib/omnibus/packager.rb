37a329158e8073ac886409766bafba4d3bd6b79e
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2024-01-09 16:04:57 +0100

    Implement packager for remote updater OCI-compliant packages

--- lib/omnibus/packager.rb
+++ lib/omnibus/packager.rb
@@ -21,6 +21,7 @@ module Omnibus
 
     autoload :Base,     "omnibus/packagers/base"
     autoload :BFF,      "omnibus/packagers/bff"
+    autoload :OCIRU,    "omnibus/packagers/ociru"
     autoload :DEB,      "omnibus/packagers/deb"
     autoload :Makeself, "omnibus/packagers/makeself"
     autoload :MSI,      "omnibus/packagers/msi"
@@ -38,7 +39,7 @@ module Omnibus
     # @return [Hash<String, Class>]
     #
     PLATFORM_PACKAGER_MAP = {
-      "debian" => DEB,
+      "debian" => [DEB, OCIRU],
       "fedora" => RPM,
       "suse" => RPM,
       "rhel" => RPM,
