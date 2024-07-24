e6c5724abd2624c93760a8cf071119262819ccdc
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-07-24 09:49:48 +0200

    attempt to fix log

--- lib/omnibus/file_syncer.rb
+++ lib/omnibus/file_syncer.rb
@@ -90,7 +90,7 @@ module Omnibus
           includes.none? { |include| File.fnmatch?(include, basename, File::FNM_DOTMATCH) }
         end
       end
-      log.info(log_key) { "Syncing: #{source_files}"}
+      Omnibus.logger.info(log_key) { "Syncing: #{source_files}"}
 
       source_files
     end
