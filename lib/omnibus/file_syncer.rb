6a8bb5d85495249bad5825ef9dcdc5ab4d3ccebb
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-07-24 09:28:08 +0200

    debug: print list of files to sync

--- lib/omnibus/file_syncer.rb
+++ lib/omnibus/file_syncer.rb
@@ -90,6 +90,7 @@ module Omnibus
           includes.none? { |include| File.fnmatch?(include, basename, File::FNM_DOTMATCH) }
         end
       end
+      log.info(log_key) { "Syncing: #{source_files}"}
 
       source_files
     end
