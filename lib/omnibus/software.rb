4865e1893ff5559c386d435dbe0b1e4a0cb1595f
Pythyu <antoine.zellmeyer@datadoghq.com>
2024-09-17 22:21:16 -0400

    feat(solaris): useless origin flag removed

--- lib/omnibus/software.rb
+++ lib/omnibus/software.rb
@@ -741,7 +741,7 @@ module Omnibus
           elsif platform_version.satisfies?(">= 5.11")
             solaris_flags = {
               "CC" => "gcc -m64 -static-libgcc",
-              "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -static-libgcc,-z,origin",
+              "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib -static-libgcc",
               "CFLAGS" => "-I#{install_dir}/embedded/include -O2",
             }
           end
