76a89c2253f3ecf3ab81255a6da475468208a1ea
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2024-12-18 12:04:30 +0100

    removing too verbose logs

--- lib/omnibus/digestable.rb
+++ lib/omnibus/digestable.rb
@@ -121,7 +121,6 @@ module Omnibus
     def update_with_file_contents(digest, filename)
       File.open(filename) do |io|
         while (chunk = io.read(1024 * 8))
-          log.debug(log_key) { "Using file chunk: #{chunk}-----"}
           digest.update(chunk)
         end
       end
