31be35f513f48a9e239475907a93f84b673f9fb8
AliDatadog <ali.benabdallah@datadoghq.com>
2025-01-23 11:54:10 +0100

    Replace git clone by git fetch to the right version

--- lib/omnibus/fetchers/git_fetcher.rb
+++ lib/omnibus/fetchers/git_fetcher.rb
@@ -151,8 +151,14 @@ module Omnibus
     # @return [void]
     #
     def git_clone
-      retry_block("git clone", [CommandTimeout, CommandFailed]) do
-        git("clone#{" --recursive" if clone_submodules?} #{source_url} .")
+      retry_block("custom git clone", [CommandTimeout, CommandFailed]) do
+        git("init")
+        git("remote add origin #{source_url}")
+        git("fetch origin #{resolved_version}")
+        git("checkout #{resolved_version}")
+        if clone_submodules?
+          git("submodule update --init --recursive")
+        end
       end
     end
 
