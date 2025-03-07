1386d59b0c22cbe026bc6cf99625e15a68f01874
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-03-07 12:06:48 +0100

    add a comment to clarify the logic

--- lib/omnibus/cli.rb
+++ lib/omnibus/cli.rb
@@ -81,6 +81,9 @@ module Omnibus
       project = Project.load(name, manifest)
       say("Building #{project.name} #{project.build_version}...")
       # Not sure why we need the !Omnibus::S3Cache.fetch_missing.empty? check here
+      # We only want to populate the cache when we actually can. In some cases, we're only reading anonymously
+      # from the cache bucket (mostly when the build isn't ran on gitlab). When that is the case, we want to disable
+      # the cache population since it would fail.
       Omnibus::S3Cache.populate if Omnibus::Config.use_s3_caching && Omnibus::Config.s3_authenticated_download && !Omnibus::S3Cache.fetch_missing.empty?
       Omnibus::S3LicenseCache.populate if Omnibus::Config.use_s3_caching && Omnibus::Config.s3_authenticated_download
       project.download
