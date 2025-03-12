784a331c0ca4f2638f8ee48698f5948ff389b93e
Kylian Serrania <kylian.serrania@datadoghq.com>
2025-03-12 13:30:07 +0100

    Add :force_fetch PathFetcher option

--- lib/omnibus/fetchers/path_fetcher.rb
+++ lib/omnibus/fetchers/path_fetcher.rb
@@ -21,15 +21,14 @@ module Omnibus
     @@source_path_mutexes = Hash.new { |h, k| h[k] = Mutex.new }
 
     #
-    # Always fetch the local directory
-    # HACK: compared to upstream, makes fetching faster due to not having to compute hashes of large directories
-    # The downside of this is potentially copying sources using the PathFetcher when it is not needed (because of no changes)
-    # but this should be rare in Datadog projects, since the main use of the PathFetcher is for the git repository of the project.
+    # Fetch if the local directory checksum is different than the path directory
+    # checksum, or if the force_fetch option is set.
     #
     # @return [true, false]
     #
     def fetch_required?
-      true
+      return true if source[:force_fetch]
+      target_shasum != destination_shasum
     end
 
     #
