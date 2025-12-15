fcceb7a930e0ed8f20b2a9a792e524221d0e3573
Hugo Beauzée-Luyssen <hugo.beauzee@datadoghq.com>
2025-12-15 16:00:28 +0100

    add yet another pattern

--- lib/omnibus/builder.rb
+++ lib/omnibus/builder.rb
@@ -237,7 +237,7 @@ module Omnibus
       command(configure_cmd, options)
 
       build_commands << BuildCommand.new("Copying config.h...") do
-        candidates = ["config.h", File.join("include", "config.h"), "pyconfig.h", "libxml/xmlversion.h"]
+        candidates = ["config.h", File.join("include", "config.h"), "pyconfig.h", "libxml/xmlversion.h", File.join("build", "config.h")]
         for c in candidates do
           config_h = File.join(software.project_dir, c)
           if File.exists? config_h
