b8c29a6e71c3a36d88d2b60eb79216b97410d831
Alex Lopez <alex.lopez@datadoghq.com>
2026-07-24 15:35:04 +0200

    Make macos health check broader

--- lib/omnibus/health_check.rb
+++ lib/omnibus/health_check.rb
@@ -491,7 +491,9 @@ module Omnibus
       bad_libs = {}
       install_name = nil
 
-      yield_shellout_results("find #{project.install_dir} -type f | egrep '\.(dylib|bundle)$' | xargs otool -L") do |line|
+      # Collect info from all binaries (based on mime-type), try to do so efficiently
+      # `grep -v (` is meant to skip architecture-specific entries
+      yield_shellout_results("find #{project.install_dir} -type f -print0 | xargs -0 -n1000 -P4 file -n --mime-type \{\} \+ | grep application/x-mach-binary | grep -v '(' | cut -d':' -f 1 | xargs otool -L -arch #{Ohai['kernel']['machine']}") do |line|
         case line
         when /^(.+):$/
           current_library = Regexp.last_match[1]
